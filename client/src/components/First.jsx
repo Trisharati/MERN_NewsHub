226e9ca70bcbd769158098cf32d2c716e8768f3d
Trisharati Pal <86723529+Trisharati@users.noreply.github.com>
2023-08-28 17:25:08 +0530

    navbar change

--- client/src/components/First.jsx
+++ client/src/components/First.jsx
@@ -15,7 +15,7 @@ localStorage.clear()
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item"><Link className="nav-link " aria-current="page" to="/register" 
+                        <li className="nav-item"><Link className="nav-link" to="/register" 
                         onClick={props.onSignUp}><b>Register</b></a></li>
                         <li className="nav-item"><Link className="nav-link" to="/login"><b>Login</b></a></li>
                     </ul>
