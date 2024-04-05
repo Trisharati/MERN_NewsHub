3e381eca4b1dc4aeb55c6e46f2d157312fa8d444
Trisharati <trisharati.4@gmail.com>
2024-04-06 00:43:54 +0530

    anchor to link

--- client/src/components/First.jsx
+++ client/src/components/First.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const First = (props) => {
 
 localStorage.clear()
@@ -14,9 +14,9 @@ localStorage.clear()
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item"><a className="nav-link " aria-current="page" href="/register" 
-                        onClick={props.onSignUp}><b>Register</b></a></li>
-                        <li className="nav-item"><a className="nav-link" href="/login"><b>Login</b></a></li>
+                        <li className="nav-item"><Link className="nav-link " aria-current="page" to="/register" 
+                        onClick={props.onSignUp}><b>Register</b></Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/login"><b>Login</b></Link></li>
                     </ul>
                 </div>
             </div>
